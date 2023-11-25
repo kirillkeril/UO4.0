144670714c417a5212da477e5e78d251a2853908
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-11-25 09:59:31 +0300

    add logs

--- back/src/appeal/appeal.rmq.controller.ts
+++ back/src/appeal/appeal.rmq.controller.ts
@@ -1,7 +1,7 @@
 import {Controller} from '@nestjs/common';
 import {AppealService} from './appeal.service';
-import {Ctx, MessagePattern, Payload, RmqContext} from "@nestjs/microservices";
-import {Appeal} from "./entities/appeal.entity";
+import {Ctx, MessagePattern, Payload, RmqContext, Transport} from "@nestjs/microservices";
+import {Appeal, AppealDocument} from "./entities/appeal.entity";
 import {ObjectId} from "mongoose";
 
 @Controller()
@@ -9,23 +9,23 @@ export class AppealRmqController {
     constructor(private readonly appealService: AppealService) {
     }
 
-    // @MessagePattern("appeal_created", Transport.RMQ)
-    // async handleAppeals(@Payload() data: AppealDocument, @Ctx() context: RmqContext) {
-    //     const channel = context.getChannelRef();
-    //     const originalMsg = context.getMessage()
-    //     try {
-    //         // TODO вызываем модель классификации -> возврат значений модели
-    //         const res = data;
-    //         console.log(res);
-    //         await this.appealService.handle(res._id.toString());
-    //         // await channel.ack(originalMsg);
-    //         // TODO обновить по id как??
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    @MessagePattern("appeal_created", Transport.RMQ)
+    async handleAppeals(@Payload() data: AppealDocument, @Ctx() context: RmqContext) {
+        const channel = context.getChannelRef();
+        const originalMsg = context.getMessage()
+        try {
+            // TODO вызываем модель классификации -> возврат значений модели
+            const res = data;
+            console.log(res);
+            await this.appealService.handle(res._id.toString());
+            // await channel.ack(originalMsg);
+            // TODO обновить по id как??
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    @MessagePattern("appeal_handled")
+    @MessagePattern("appeal_handled", Transport.RMQ)
     async saveAppeal(@Payload() data: Appeal & ObjectId, @Ctx() context: RmqContext) {
         console.log("appeal saved", data);
     }
